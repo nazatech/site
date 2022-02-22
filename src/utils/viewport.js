008175ed777a320882714e9eef054cf9217041cc
azabraao <az.abraao@gmail.com>
2022-02-22 11:15:21 -0300

    prevent ssr window error

--- src/utils/viewport.js
+++ src/utils/viewport.js
@@ -1,5 +1,11 @@
-export const isMobile = window?.matchMedia(`(max-width: 768px)`)?.matches;
-export const isTablet = window?.matchMedia(
-  `(min-width: 768px) and (max-width: 1023px)`
-).matches;
-export const isDesktop = window?.matchMedia(`(min-width: 1024px)`)?.matches;
+const isBrowser = typeof window !== "undefined";
+
+export const isMobile = isBrowser
+  ? window?.matchMedia(`(max-width: 768px)`)?.matches
+  : false;
+export const isTablet = isBrowser
+  ? window?.matchMedia(`(min-width: 768px) and (max-width: 1023px)`).matches
+  : false;
+export const isDesktop = isBrowser
+  ? window?.matchMedia(`(min-width: 1024px)`)?.matches
+  : false;
