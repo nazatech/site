4f511e28fcc4066313e1e4a1f9d3b3310bf515a3
azabraao <az.abraao@gmail.com>
2022-02-21 22:14:16 -0300

    handle window undefined

--- src/hooks/useIsMobile.js
+++ src/hooks/useIsMobile.js
@@ -1,34 +1,11 @@
-import { useEffect, useState } from "react";
+const isBrowser = typeof window !== "undefined";
 
 const useIsMobile = (mobileScreenSize = 768) => {
-  if (typeof window.matchMedia !== "function") {
-    throw Error("matchMedia not supported by browser!");
-  }
+  const mediaListener = isBrowser
+    ? window?.matchMedia(`(max-width: ${mobileScreenSize}px)`)
+    : {};
 
-  const mediaListener = window.matchMedia(`(max-width: ${mobileScreenSize}px)`);
-  const [isMobile, setIsMobile] = useState(mediaListener.matches);
-
-  const checkIsMobile = (event) => {
-    setIsMobile(event.matches);
-  };
-
-  useEffect(() => {
-    // try catch used to fallback for browser compatibility
-    try {
-      mediaListener.addEventListener("change", checkIsMobile);
-    } catch {
-      mediaListener.addListener(checkIsMobile);
-    }
-
-    return () => {
-      try {
-        mediaListener.removeEventListener("change", checkIsMobile);
-      } catch {
-        mediaListener.removeListener(checkIsMobile);
-      }
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const isMobile = mediaListener?.matches;
 
   return isMobile;
 };
