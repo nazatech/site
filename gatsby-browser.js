ebb887acc1d0d9ed661ef9ea579667295400db9a
azabraao <az.abraao@gmail.com>
2022-02-22 16:24:23 -0300

    add missing raleway font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,5 @@
 import "./src/styles/global.css";
+import "@fontsource/raleway/300.css";
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/700.css";
+import "@fontsource/raleway/800.css";
