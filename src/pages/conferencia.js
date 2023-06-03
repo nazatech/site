b0079b9836970cf351704991c50f076c9f784344
Abraão Azevedo <az.abraao@gmail.com>
2023-06-03 10:27:04 -0300

    add user-scalable=no to conferencia page

--- src/pages/conferencia.js
+++ src/pages/conferencia.js
@@ -7,7 +7,7 @@ const Conferencia = () => {
       <Helmet>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </Helmet>
       <iframe
