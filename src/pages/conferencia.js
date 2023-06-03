2363017046f56a62d85f14e4df574b96234d9aff
Abraão Azevedo <az.abraao@gmail.com>
2023-06-03 10:20:23 -0300

    disable zoom from conferencia page

--- src/pages/conferencia.js
+++ src/pages/conferencia.js
@@ -1,17 +1,26 @@
 import React, { memo } from "react";
+import { Helmet } from "react-helmet";
 
 const Conferencia = () => {
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSeeKYh15UIahiBo3baEvpzeEilHEh_a48un-Tk-cPhI_pKFRA/viewform?embedded=true"
-      className="w-screen h-screen"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-      title="Formulário de inscrição para a conferência MNI"
-    >
-      Carregando…
-    </iframe>
+    <>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </Helmet>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeeKYh15UIahiBo3baEvpzeEilHEh_a48un-Tk-cPhI_pKFRA/viewform?embedded=true"
+        className="w-screen h-screen"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="Formulário de inscrição para a conferência MNI"
+      >
+        Carregando…
+      </iframe>
+    </>
   );
 };
 
